67fb2a384cc5bc8cbf88c461049cbbef4c089e42
sou1118 <hello@kattyan.dev>
2023-08-29 12:23:56 +0900

    fix: step16

--- net.c
+++ net.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "arp.h"
 #include "platform.h"
@@ -22,10 +23,18 @@ struct net_protocol_queue_entry {
     uint8_t data[];
 };
 
+struct net_timer {
+    struct net_timer *next;
+    struct timeval interval;
+    struct timeval last;
+    void (*handler)(void);
+};
+
 /* NOTE: if you want to add/delete the entries after net_run(), you need to
  * protect these lists with a mutex. */
 static struct net_device *devices;
 static struct net_protocol *protocols;
+static struct net_timer *timers;
 
 struct net_device *net_device_alloc(void) {
     struct net_device *dev;
@@ -157,6 +166,45 @@ int net_protocol_register(uint16_t type,
     proto->next = protocols;
     protocols = proto;
     infof("registered, type=0x%04x", type);
+    return 0;
+}
+
+/* NOTE: must not be call after net_run() */
+int net_timer_register(struct timeval interval, void (*handler)(void)) {
+    struct net_timer *timer;
+
+    // Allocate memory for the timer
+    timer = memory_alloc(sizeof(*timer));
+    if (!timer) {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+
+    // Initialize the timer
+    timer->interval = interval;
+    gettimeofday(&timer->last, NULL);
+    timer->handler = handler;
+
+    // Add the timer to the global list
+    timer->next = timers;
+    timers = timer;
+    infof("registered: interval={%d, %d}", interval.tv_sec, interval.tv_usec);
+    return 0;
+}
+
+int net_timer_handler(void) {
+    struct net_timer *timer;
+    struct timeval now, diff;
+
+    for (timer = timers; timer; timer = timer->next) {
+        gettimeofday(&now, NULL);
+        timersub(&now, &timer->last, &diff);
+        if (timercmp(&timer->interval, &diff, <) !=
+            0) { /* true (!0) or false (0) */
+            timer->handler();
+            timer->last = now;
+        }
+    }
     return 0;
 }
 
